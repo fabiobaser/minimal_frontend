4b2d22e00bdfec9cdd04411a6b099c3251b7daa4
Fabio Baser <f.baser@monday-consulting.com>
2023-01-13 14:43:22 +0100

    added tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import React from "react";
 function App() {
     return (
         <div>
-            <h2>Hallo aus der App</h2>
+            <h2 className="text-xl">Hallo aus der App</h2>
+            <button
+                type="button"
+                className="inline-flex items-center px-5 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+                Click here
+            </button>
         </div>
     );
 }
